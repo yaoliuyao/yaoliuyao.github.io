212457e5fdfc6bcd2b89b6c7dc5bcd1aa8953e27
yaoliusan <yaoliusan@gmail.com>
2021-06-02 02:20:20 +0000

    best_try_and_best_chance

--- .src/private-custom.el
+++ .src/private-custom.el
@@ -55,4 +55,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-meta-line ((t (:inherit nil :foreground "gainsboro")))))
