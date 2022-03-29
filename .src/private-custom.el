e39f16952b080a00f7cf82bba404563c3b90d03f
yaoliuyao <yaoliuyao@gmail.com>
2022-03-29 01:21:48 +0000

    best_try_and_best_chance

--- .src/private-custom.el
+++ .src/private-custom.el
@@ -18,7 +18,15 @@
 
 (setq ic/faces (list :frame 'max
                      :theme nil
-                     :font (cons "Consolas" 120)))
+                     :font (cons "Consolas" 120)
+                     :font-unicode (cons "思源宋体 CN" 120)))
+
+(setq lsp-headerline-arrow
+      #(">" 0 1
+        (face #1=(:family "Material Icons" :height 1.2 :inherit lsp-headerline-breadcrumb-separator-face)
+              font-lock-face #1# display
+              (raise 0)
+              rear-nonsticky t)))
 
 ;;(setq-default left-fringe-width 20)
 
@@ -123,11 +131,13 @@
  '(custom-safe-themes
    '("1ca05bdae217adeb636e9bc5e84c8f1d045be2c8004fafd5337d141d9b67a96f" default))
  '(package-selected-packages
-   '(sis rime nano-theme pyim-basedict systemd modus-themes dockerfile-mode docker go-mode php-mode know-your-http-well auctex citre cape corfu keycast rainbow-mode blackout leaf-keywords leaf editorconfig org-roam-ui sql-indent android-mode sharper fsharp-mode eshell-outline cowsay poly-markdown poly-org polymode edit-indirect evil macrostep license-templates gitignore-templates org-contrib git-modes sx cmake-mode httprepl company-tabnine consult-eglot orderless vertico lsp-java tide lsp-ui lsp-treemacs dap-mode ace-window magit marginalia multi-line go-translate bbdb erc-hl-nicks org-noter-pdftools org-noter which-key ztree nnreddit omnisharp eglot embark-consult csproj-mode clojure-mode all-the-icons mpv udev-mode pyim ssh-agency yaml-mode xterm-color websocket web-mode web-beautify vlf uuidgen use-package syntax-subword session scala-mode sbt-mode sass-mode robe rg rcirc-styles rainbow-delimiters powershell plantuml-mode page-break-lines ox-pandoc org-roam-server org-present org-plus-contrib ob-restclient oauth memory-usage markdown-toc lua-mode kotlin-mode json-mode jdecomp htmlize hindent hide-mode-line groovy-mode graphviz-dot-mode gnuplot git-timemachine folding fold-this expand-region erlang engine-mode emms emmet-mode elpy elfeed-org dired-dups delight dante csharp-mode cquery company-web company-restclient company-posframe company-php company-math company-go company-ghc company-auctex c-eldoc attrap alchemist)))
+   '(kubernetes color-moccur hide-lines gruvbox-theme sis rime nano-theme pyim-basedict systemd modus-themes dockerfile-mode docker go-mode php-mode know-your-http-well auctex citre cape corfu keycast rainbow-mode blackout leaf-keywords leaf editorconfig org-roam-ui sql-indent android-mode sharper fsharp-mode eshell-outline cowsay poly-markdown poly-org polymode edit-indirect evil macrostep license-templates gitignore-templates org-contrib git-modes sx cmake-mode httprepl company-tabnine consult-eglot orderless vertico lsp-java tide lsp-ui lsp-treemacs dap-mode ace-window magit marginalia multi-line go-translate bbdb erc-hl-nicks org-noter-pdftools org-noter which-key ztree nnreddit omnisharp eglot embark-consult csproj-mode clojure-mode all-the-icons mpv udev-mode pyim ssh-agency yaml-mode xterm-color websocket web-mode web-beautify vlf uuidgen use-package syntax-subword session scala-mode sbt-mode sass-mode robe rg rcirc-styles rainbow-delimiters powershell plantuml-mode page-break-lines ox-pandoc org-roam-server org-present org-plus-contrib ob-restclient oauth memory-usage markdown-toc lua-mode kotlin-mode json-mode jdecomp htmlize hindent hide-mode-line groovy-mode graphviz-dot-mode gnuplot git-timemachine folding fold-this expand-region erlang engine-mode emms emmet-mode elpy elfeed-org dired-dups delight dante csharp-mode cquery company-web company-restclient company-posframe company-php company-math company-go company-ghc company-auctex c-eldoc attrap alchemist)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:family "Courier New"))))
+ '(markdown-pre-face ((t (:inherit markdown-code-face :extend t :background "light yellow" :foreground "midnight blue"))))
  '(org-meta-line ((t (:inherit nil :foreground "gainsboro")))))
